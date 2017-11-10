3a17efe7204151995c69f76ddf8ceee7584fcd33
Rodney R <majid.muhassan@gmail.com>
2017-11-10 01:23:54 +0000

    switched things up this time around.  I split the time between fuction and appearance.  There are still bugs to be worked on both appearance and function.  The visibility of the timer is an issue. I want it in the jumbotron, however it's hard to see with the color scheme. I am considering floating to the bottom right of the jumbotron.  I also want the text on the start button to say Cipher when it's moused over. There needs to be a bit more padding where the answers are. For the fucntionality,the clock messes up if the start button is pushed multiple times.  There needs to be something borken in my loop causing this to happen.  I still have to create and link my true and false conditions as well as create the code to show a result for the ansewers that are correct and wrong

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+ // Timer has bugs
+ $(document).on('click', "#button", function() {
+  var time = 45;
+
+  function redirect() {
+   var id = setTimeout(redirect, 1000); // function will fired for every one second
+   $("#timer").html(time);
+   if (time == 0) {
+    // if time is zero redirect
+    clearTimeout(id);
+    $("#timer2").html("Game Over")
+   }
+
+   time--; // the the time decrease, from 30 to 0
+  }
+
+  // remember to call function redirect
+  redirect();
+ });
+
+
+//example of what can be done to access the id's on the other
+
+ // <div class="choice" data-value = 0>Charlotte</div>
+ // <div class="choice">Raleigh</div>
+ // <div class="choice">Greensboro</div>
+
+
+ // $(".choice").click(function(){
+
+
+ //  var val = $(this).attr('data-value'); //0
+
+ // })
